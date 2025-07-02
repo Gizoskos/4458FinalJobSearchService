97594df6b4234fd8a708e561c80b8685586e6c9d
Gizoskos <gizemgultoprak2004@gmail.com>
2025-07-03 00:24:51 +0300

    job search

--- src/main/java/com/gizem/jobsearchservice/JobSearchServiceApplication.java
+++ src/main/java/com/gizem/jobsearchservice/JobSearchServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class JobSearchServiceApplication {
 
     public static void main(String[] args) {
